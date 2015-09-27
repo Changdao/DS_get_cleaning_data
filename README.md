513b1a672e10d863555252317acba86cca06503d
Changdao <jameslau@263.net>
2015-09-27 10:15:01 +0800

    add comments on the run_anaylysis

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 Explanation
 ====================
-The analysis process is as following: 
-Read train set data x_test.txt in fixed width, as well as as 561 columns. 
-Read train set data subject_test.txt. 
-Read train set y_test. 
-Merge those three datasets. 
-
-Do the above steps on the test sets.
-Merge the train set with test set.
-Aggregate the independent tidy data set with the average of each variable for each activity and each subject.
-
-Write the data to file.
+The analysis process is as following:    
+1. Read train set data x_test.txt in fixed width, as well as as 561 columns.  
+2. Read train set data subject_test.txt.  
+3. Read train set y_test.  
+4. Merge those three datasets.  
+5. Do the above steps on the test sets.   
+6. Merge the train set with test set.   
+7. Aggregate the independent tidy data set with the average of each variable for each activity and each subject.   
+8. Write the data to file.   
--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,28 @@
+##read the features description file for the "names" of the data.frame
 features<-read.table('UCI HAR Dataset/features.txt')
+## read and process trainset dataset.
 trainTable1<-read.table('UCI HAR Dataset/train/X_train.txt',colClasses='numeric')
+## assign names to the train data
 names(trainTable1)<-as.vector(features$V2)
-#trainTable1$setType<-'train'
 trainTable2<-read.table('UCI HAR Dataset/train/Y_train.txt',colClasses='character')
 names(trainTable2)<-c('activity')
 trainTable3<-read.table('UCI HAR Dataset/train/subject_train.txt',colClasses='integer')
 names(trainTable3)<-c('subject')
+## merge the trainset
 trainTable<-cbind(trainTable1,trainTable2,trainTable3);
 
-
+## read and process the test set.
 testTable1<-read.table('UCI HAR Dataset/test/X_test.txt',colClasses='numeric')
 names(testTable1)<-as.vector(features$V2)
-#testTable1$setType<-'test'
 testTable2<-read.table('UCI HAR Dataset/test/Y_test.txt',colClasses='character')
 names(testTable2)<-c('activity')
 testTable3<-read.table('UCI HAR Dataset/test/subject_test.txt',colClasses='integer')
 names(testTable3)<-c('subject')
+##merge the test set.
 testTable<-cbind(testTable1,testTable2,testTable3);
-
+## mert the train set with the test set.
 totalData<-rbind(trainTable,testTable)
-#totalData<-testTable
 
+## aggregate the totoal data to generate the tidy data.
 tidyData <- aggregate( . ~ activity + subject, data = totalData, mean)
 write.table(tidyData,file='tidy.txt',row.name=FALSE)
